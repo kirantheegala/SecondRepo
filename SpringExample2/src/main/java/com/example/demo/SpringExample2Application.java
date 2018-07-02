66cdbf3c2bb0d0074b4635ada2b43777a870805d
kirantheegala <kirantheegala@gmail.com>
2018-07-02 12:15:35 -0500

    Initial Commit

---
+++ SpringExample2/src/main/java/com/example/demo/SpringExample2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExample2Application.class, args);
+	}
+}
